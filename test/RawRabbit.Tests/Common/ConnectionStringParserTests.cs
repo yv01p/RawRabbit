3551b1d946af73e8967dca7eba564c16641cf4c9
Peter Zadrozny <246755631+yv01p@users.noreply.github.com>
2026-05-13 13:33:25 +0000

    Clean A25 anti-patterns from ConnectionStringParserTests; consolidate via Theory

--- test/RawRabbit.Tests/Common/ConnectionStringParserTests.cs
+++ test/RawRabbit.Tests/Common/ConnectionStringParserTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using RawRabbit.Common;
 using Xunit;
 
@@ -6,350 +6,77 @@ namespace RawRabbit.Tests.Common
 {
 	public class ConnectionStringParserTests
 	{
-		[Fact]
-		public void Should_Be_Able_To_Parse_ConnectionString_Without_Credentials()
+		[Theory]
+		[InlineData("host", "guest", "guest", "/", "host", 5672)]
+		[InlineData("host:1234", "guest", "guest", "/", "host", 1234)]
+		[InlineData("host/virtualHost", "guest", "guest", "virtualHost", "host", 5672)]
+		[InlineData("host:1234/virtualHost", "guest", "guest", "virtualHost", "host", 1234)]
+		[InlineData("username:password@host1,host2", "username", "password", "/", "host1", 5672)]
+		[InlineData("username:password@host1,host2/virtualHost", "username", "password", "virtualHost", "host1", 5672)]
+		[InlineData("username:password@host1,host2:1234", "username", "password", "/", "host1", 1234)]
+		[InlineData("username:password@host1,host2:1234/virtualHost", "username", "password", "virtualHost", "host1", 1234)]
+		public void Should_Parse_Connection_String_With_Various_Hosts_And_Credentials(string connectionString, string username, string password, string virtualHost, string firstHost, int port)
 		{
-			/* Setup */
-			const string connectionString = "host";
-
-			/* Test */
 			var config = ConnectionStringParser.Parse(connectionString);
 
-			/* Assert */
-			Assert.Equal(expected: "guest", actual: config.Username);
-			Assert.Equal(expected: "guest", actual: config.Password);
-			Assert.Equal(expected: "/", actual: config.VirtualHost);
-			Assert.Equal(expected: "host", actual: config.Hostnames[0]);
-			Assert.Equal(expected: 5672, actual: config.Port);
+			Assert.Equal(username, config.Username);
+			Assert.Equal(password, config.Password);
+			Assert.Equal(virtualHost, config.VirtualHost);
+			Assert.Equal(firstHost, config.Hostnames[0]);
+			Assert.Equal(port, config.Port);
 		}
 
-		[Fact]
-		public void Should_Be_Able_To_Parse_ConnectionString_Without_Credentials_With_Port()
+		[Theory]
+		[InlineData("host1,host2?requestTimeout=10&publishConfirmTimeout=20&recoveryInterval=30&autoCloseConnection=false&persistentDeliveryMode=false&automaticRecovery=false&topologyRecovery=false",
+			"guest", "guest", "/", "host1", "host2", 5672, false, false, false, false)]
+		[InlineData("username:password@host1,host2:1234?requestTimeout=10&publishConfirmTimeout=20&recoveryInterval=30&autoCloseConnection=false&persistentDeliveryMode=false&automaticRecovery=false&topologyRecovery=false",
+			"username", "password", "/", "host1", "host2", 1234, false, false, false, false)]
+		[InlineData("username:password@host1,host2:1234/virtualHost?requestTimeout=10&publishConfirmTimeout=20&recoveryInterval=30&autoCloseConnection=false&persistentDeliveryMode=false&automaticRecovery=false&topologyRecovery=false",
+			"username", "password", "virtualHost", "host1", "host2", 1234, false, false, false, false)]
+		[InlineData("host1,host2:1234/virtualHost?requestTimeout=10&publishConfirmTimeout=20&recoveryInterval=30&autoCloseConnection=false&persistentDeliveryMode=false&automaticRecovery=false&topologyRecovery=false",
+			"guest", "guest", "virtualHost", "host1", "host2", 1234, false, false, false, false)]
+		[InlineData("username:password@host1,host2?requestTimeout=10&publishConfirmTimeout=20&recoveryInterval=30&autoCloseConnection=false&persistentDeliveryMode=false&automaticRecovery=false&topologyRecovery=false",
+			"username", "password", "/", "host1", "host2", 5672, false, false, false, false)]
+		public void Should_Parse_Multi_Host_Connection_String_With_Parameters(
+			string connectionString, string username, string password, string virtualHost,
+			string firstHost, string secondHost, int port,
+			bool autoClose, bool persistDelivery, bool autoRecovery, bool topoRecovery)
 		{
-			/* Setup */
-			const string connectionString = "host:1234";
-
-			/* Test */
 			var config = ConnectionStringParser.Parse(connectionString);
 
-			/* Assert */
-			Assert.Equal(expected: "guest", actual: config.Username);
-			Assert.Equal(expected: "guest", actual: config.Password);
-			Assert.Equal(expected: "/", actual: config.VirtualHost);
-			Assert.Equal(expected: "host", actual: config.Hostnames[0]);
-			Assert.Equal(expected: 1234, actual: config.Port);
-		}
-
-		[Fact]
-		public void Should_Be_Able_To_Parse_ConnectionString_Without_Credentials_With_VirtualHost()
-		{
-			/* Setup */
-			const string connectionString = "host/virtualHost";
-
-			/* Test */
-			var config = ConnectionStringParser.Parse(connectionString);
-
-			/* Assert */
-			Assert.Equal(expected: "guest", actual: config.Username);
-			Assert.Equal(expected: "guest", actual: config.Password);
-			Assert.Equal(expected: "virtualHost", actual: config.VirtualHost);
-			Assert.Equal(expected: "host", actual: config.Hostnames[0]);
-			Assert.Equal(expected: 5672, actual: config.Port);
-		}
-
-		[Fact]
-		public void Should_Be_Able_To_Parse_ConnectionString_Without_Credentials_With_Parameters()
-		{
-			/* Setup */
-			const string connectionString = "host1,host2?" +
-											"requestTimeout=10" +
-											"&publishConfirmTimeout=20" +
-											"&recoveryInterval=30" +
-											"&autoCloseConnection=false" +
-											"&persistentDeliveryMode=false" +
-											"&automaticRecovery=false" +
-											"&topologyRecovery=false";
-
-			/* Test */
-			var config = ConnectionStringParser.Parse(connectionString);
-
-			/* Assert */
-			Assert.Equal(expected: "guest", actual: config.Username);
-			Assert.Equal(expected: "guest", actual: config.Password);
-			Assert.Equal(expected: "/", actual: config.VirtualHost);
-			Assert.Equal(expected: "host1", actual: config.Hostnames[0]);
-			Assert.Equal(expected: "host2", actual: config.Hostnames[1]);
-			Assert.Equal(expected: 5672, actual: config.Port);
-			Assert.Equal(expected: TimeSpan.FromSeconds(10), actual: config.RequestTimeout);
-			Assert.Equal(expected: TimeSpan.FromSeconds(20), actual: config.PublishConfirmTimeout);
-			Assert.Equal(expected: TimeSpan.FromSeconds(30), actual: config.RecoveryInterval);
-			Assert.Equal(expected: false, actual: config.AutoCloseConnection);
-			Assert.Equal(expected: false, actual: config.PersistentDeliveryMode);
-			Assert.Equal(expected: false, actual: config.AutomaticRecovery);
-			Assert.Equal(expected: false, actual: config.TopologyRecovery);
-		}
-
-		[Fact]
-		public void Should_Be_Able_To_Parse_ConnectionString_Without_Credentials_With_Port_And_VirtualHost()
-		{
-			/* Setup */
-			const string connectionString = "host:1234/virtualHost";
-
-			/* Test */
-			var config = ConnectionStringParser.Parse(connectionString);
-
-			/* Assert */
-			Assert.Equal(expected: "guest", actual: config.Username);
-			Assert.Equal(expected: "guest", actual: config.Password);
-			Assert.Equal(expected: "virtualHost", actual: config.VirtualHost);
-			Assert.Equal(expected: "host", actual: config.Hostnames[0]);
-			Assert.Equal(expected: 1234, actual: config.Port);
-		}
-
-		[Fact]
-		public void Should_Be_Able_To_Parse_ConnectionString_Without_Port()
-		{
-			/* Setup */
-			const string connectionString = "username:password@host1,host2";
-
-			/* Test */
-			var config = ConnectionStringParser.Parse(connectionString);
-
-			/* Assert */
-			Assert.Equal(expected: "username", actual: config.Username);
-			Assert.Equal(expected: "password", actual: config.Password);
-			Assert.Equal(expected: "/", actual: config.VirtualHost);
-			Assert.Equal(expected: "host1", actual: config.Hostnames[0]);
-			Assert.Equal(expected: "host2", actual: config.Hostnames[1]);
-			Assert.Equal(expected: 5672, actual: config.Port);
-		}
-
-		[Fact]
-		public void Should_Be_Able_To_Parse_ConnectionString_Without_Port_With_VirtualHost()
-		{
-			/* Setup */
-			const string connectionString = "username:password@host1,host2/virtualHost";
-
-			/* Test */
-			var config = ConnectionStringParser.Parse(connectionString);
-
-			/* Assert */
-			Assert.Equal(expected: "username", actual: config.Username);
-			Assert.Equal(expected: "password", actual: config.Password);
-			Assert.Equal(expected: "virtualHost", actual: config.VirtualHost);
-			Assert.Equal(expected: "host1", actual: config.Hostnames[0]);
-			Assert.Equal(expected: "host2", actual: config.Hostnames[1]);
-			Assert.Equal(expected: 5672, actual: config.Port);
-		}
-
-		[Fact]
-		public void Should_Be_Able_To_Parse_ConnectionString_With_Port_Without_VirtualHost()
-		{
-			/* Setup */
-			const string connectionString = "username:password@host1,host2:1234";
-
-			/* Test */
-			var config = ConnectionStringParser.Parse(connectionString);
-
-			/* Assert */
-			Assert.Equal(expected: "username", actual: config.Username);
-			Assert.Equal(expected: "password", actual: config.Password);
-			Assert.Equal(expected: "/", actual: config.VirtualHost);
-			Assert.Equal(expected: "host1", actual: config.Hostnames[0]);
-			Assert.Equal(expected: "host2", actual: config.Hostnames[1]);
-			Assert.Equal(expected: 1234, actual: config.Port);
-		}
-
-		[Fact]
-		public void Should_Be_Able_To_Parse_ConnectionString_With_Port_And_VirtualHost()
-		{
-			/* Setup */
-			const string connectionString = "username:password@host1,host2:1234/virtualHost";
-
-			/* Test */
-			var config = ConnectionStringParser.Parse(connectionString);
-
-			/* Assert */
-			Assert.Equal(expected: "username", actual: config.Username);
-			Assert.Equal(expected: "password", actual: config.Password);
-			Assert.Equal(expected: "virtualHost", actual: config.VirtualHost);
-			Assert.Equal(expected: "host1", actual: config.Hostnames[0]);
-			Assert.Equal(expected: "host2", actual: config.Hostnames[1]);
-			Assert.Equal(expected: 1234, actual: config.Port);
-		}
-
-		[Fact]
-		public void Should_Be_Able_To_Parse_ConnectionString_With_All_Attributes_And_Parameters()
-		{
-			/* Setup */
-			const string connectionString = "username:password@host1,host2:1234/virtualHost?" +
-											"requestTimeout=10" +
-											"&publishConfirmTimeout=20" +
-											"&recoveryInterval=30" +
-											"&autoCloseConnection=false" +
-											"&persistentDeliveryMode=false" +
-											"&automaticRecovery=false" +
-											"&topologyRecovery=false";
-
-			/* Test */
-			var config = ConnectionStringParser.Parse(connectionString);
-
-			/* Assert */
-			Assert.Equal(expected: "username", actual: config.Username);
-			Assert.Equal(expected: "password", actual: config.Password);
-			Assert.Equal(expected: "virtualHost", actual: config.VirtualHost);
-			Assert.Equal(expected: "host1", actual: config.Hostnames[0]);
-			Assert.Equal(expected: "host2", actual: config.Hostnames[1]);
-			Assert.Equal(expected: 1234, actual: config.Port);
-			Assert.Equal(expected: TimeSpan.FromSeconds(10), actual: config.RequestTimeout);
-			Assert.Equal(expected: TimeSpan.FromSeconds(20), actual: config.PublishConfirmTimeout);
-			Assert.Equal(expected: TimeSpan.FromSeconds(30), actual: config.RecoveryInterval);
-			Assert.Equal(expected: false, actual: config.AutoCloseConnection);
-			Assert.Equal(expected: false, actual: config.PersistentDeliveryMode);
-			Assert.Equal(expected: false, actual: config.AutomaticRecovery);
-			Assert.Equal(expected: false, actual: config.TopologyRecovery);
-		}
-
-		[Fact]
-		public void Should_Be_Able_To_Parse_ConnectionString_Without_VirtualHost_With_Parameters()
-		{
-			/* Setup */
-			const string connectionString = "username:password@host1,host2:1234?" +
-											"requestTimeout=10" +
-											"&publishConfirmTimeout=20" +
-											"&recoveryInterval=30" +
-											"&autoCloseConnection=false" +
-											"&persistentDeliveryMode=false" +
-											"&automaticRecovery=false" +
-											"&topologyRecovery=false";
-
-			/* Test */
-			var config = ConnectionStringParser.Parse(connectionString);
-
-			/* Assert */
-			Assert.Equal(expected: "username", actual: config.Username);
-			Assert.Equal(expected: "password", actual: config.Password);
-			Assert.Equal(expected: "/", actual: config.VirtualHost);
-			Assert.Equal(expected: "host1", actual: config.Hostnames[0]);
-			Assert.Equal(expected: "host2", actual: config.Hostnames[1]);
-			Assert.Equal(expected: 1234, actual: config.Port);
-			Assert.Equal(expected: TimeSpan.FromSeconds(10), actual: config.RequestTimeout);
-			Assert.Equal(expected: TimeSpan.FromSeconds(20), actual: config.PublishConfirmTimeout);
-			Assert.Equal(expected: TimeSpan.FromSeconds(30), actual: config.RecoveryInterval);
-			Assert.Equal(expected: false, actual: config.AutoCloseConnection);
-			Assert.Equal(expected: false, actual: config.PersistentDeliveryMode);
-			Assert.Equal(expected: false, actual: config.AutomaticRecovery);
-			Assert.Equal(expected: false, actual: config.TopologyRecovery);
-		}
-
-		[Fact]
-		public void Should_Be_Able_To_Parse_ConnectionString_Without_Credentials_With_Port_And_VirtualHost_And_Parameters()
-		{
-			/* Setup */
-			const string connectionString = "host1,host2:1234/virtualHost?" +
-											"requestTimeout=10" +
-											"&publishConfirmTimeout=20" +
-											"&recoveryInterval=30" +
-											"&autoCloseConnection=false" +
-											"&persistentDeliveryMode=false" +
-											"&automaticRecovery=false" +
-											"&topologyRecovery=false";
-
-			/* Test */
-			var config = ConnectionStringParser.Parse(connectionString);
-
-			/* Assert */
-			Assert.Equal(expected: "guest", actual: config.Username);
-			Assert.Equal(expected: "guest", actual: config.Password);
-			Assert.Equal(expected: "virtualHost", actual: config.VirtualHost);
-			Assert.Equal(expected: "host1", actual: config.Hostnames[0]);
-			Assert.Equal(expected: "host2", actual: config.Hostnames[1]);
-			Assert.Equal(expected: 1234, actual: config.Port);
-			Assert.Equal(expected: TimeSpan.FromSeconds(10), actual: config.RequestTimeout);
-			Assert.Equal(expected: TimeSpan.FromSeconds(20), actual: config.PublishConfirmTimeout);
-			Assert.Equal(expected: TimeSpan.FromSeconds(30), actual: config.RecoveryInterval);
-			Assert.Equal(expected: false, actual: config.AutoCloseConnection);
-			Assert.Equal(expected: false, actual: config.PersistentDeliveryMode);
-			Assert.Equal(expected: false, actual: config.AutomaticRecovery);
-			Assert.Equal(expected: false, actual: config.TopologyRecovery);
-		}
-
-		[Fact]
-		public void Should_Be_Able_To_Parse_ConnectionString_Without_VirtualHost_And_Port_With_Parameters()
-		{
-			/* Setup */
-			const string connectionString = "username:password@host1,host2?" +
-											"requestTimeout=10" +
-											"&publishConfirmTimeout=20" +
-											"&recoveryInterval=30" +
-											"&autoCloseConnection=false" +
-											"&persistentDeliveryMode=false" +
-											"&automaticRecovery=false" +
-											"&topologyRecovery=false";
-
-			/* Test */
-			var config = ConnectionStringParser.Parse(connectionString);
-
-			/* Assert */
-			Assert.Equal(expected: "username", actual: config.Username);
-			Assert.Equal(expected: "password", actual: config.Password);
-			Assert.Equal(expected: "/", actual: config.VirtualHost);
-			Assert.Equal(expected: "host1", actual: config.Hostnames[0]);
-			Assert.Equal(expected: "host2", actual: config.Hostnames[1]);
-			Assert.Equal(expected: 5672, actual: config.Port);
-			Assert.Equal(expected: TimeSpan.FromSeconds(10), actual: config.RequestTimeout);
-			Assert.Equal(expected: TimeSpan.FromSeconds(20), actual: config.PublishConfirmTimeout);
-			Assert.Equal(expected: TimeSpan.FromSeconds(30), actual: config.RecoveryInterval);
-			Assert.Equal(expected: false, actual: config.AutoCloseConnection);
-			Assert.Equal(expected: false, actual: config.PersistentDeliveryMode);
-			Assert.Equal(expected: false, actual: config.AutomaticRecovery);
-			Assert.Equal(expected: false, actual: config.TopologyRecovery);
+			Assert.Equal(username, config.Username);
+			Assert.Equal(password, config.Password);
+			Assert.Equal(virtualHost, config.VirtualHost);
+			Assert.Equal(firstHost, config.Hostnames[0]);
+			Assert.Equal(secondHost, config.Hostnames[1]);
+			Assert.Equal(port, config.Port);
+			Assert.Equal(TimeSpan.FromSeconds(10), config.RequestTimeout);
+			Assert.Equal(TimeSpan.FromSeconds(20), config.PublishConfirmTimeout);
+			Assert.Equal(TimeSpan.FromSeconds(30), config.RecoveryInterval);
+			Assert.Equal(autoClose, config.AutoCloseConnection);
+			Assert.Equal(persistDelivery, config.PersistentDeliveryMode);
+			Assert.Equal(autoRecovery, config.AutomaticRecovery);
+			Assert.Equal(topoRecovery, config.TopologyRecovery);
 		}
 
 		[Fact]
 		public void Should_Throw_Format_Exception_When_ConnectionString_Has_Bad_Port()
 		{
-			/* Setup */
 			const string connectionString = "username:password@host1,host2:port";
-			Exception exception = null;
 
-			/* Test */
-			try
-			{
-				ConnectionStringParser.Parse(connectionString);
-			}
-			catch (Exception e)
-			{
-				exception = e;
-			}
+			var exception = Assert.Throws<FormatException>(() => ConnectionStringParser.Parse(connectionString));
 
-			/* Assert */
-			Assert.NotNull(exception);
-			Assert.IsType(typeof(FormatException), exception);
-			Assert.Equal(expected: "The supplied port 'port' in the connection string is not a number", actual: exception.Message);
+			Assert.Equal("The supplied port 'port' in the connection string is not a number", exception.Message);
 		}
 
 		[Fact]
 		public void Should_Throw_Argument_Exception_When_ConnectionString_Has_Bad_Property()
 		{
-			/* Setup */
 			const string connectionString = "username:password@host1,host2?badproperty=true";
-			Exception exception = null;
 
-			/* Test */
-			try
-			{
-				ConnectionStringParser.Parse(connectionString);
-			}
-			catch (Exception e)
-			{
-				exception = e;
-			}
+			var exception = Assert.Throws<ArgumentException>(() => ConnectionStringParser.Parse(connectionString));
 
-			/* Assert */
-			Assert.NotNull(exception);
-			Assert.IsType(typeof(ArgumentException), exception);
-			Assert.Equal(expected: "No configuration property named 'badproperty'", actual: exception.Message);
+			Assert.Equal("No configuration property named 'badproperty'", exception.Message);
 		}
 
 	}
