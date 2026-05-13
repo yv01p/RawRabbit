b1c188c16842aca734fa3187851bc109f1a3d8d9
Peter Zadrozny <246755631+yv01p@users.noreply.github.com>
2026-05-13 00:45:01 +0000

    Re-indent Phase 4 files to tabs per .editorconfig

--- src/RawRabbit/Serialization/JsonSerializer.cs
+++ src/RawRabbit/Serialization/JsonSerializer.cs
@@ -3,41 +3,41 @@ using System.Text.Json;
 
 namespace RawRabbit.Serialization
 {
-    public class JsonSerializer : StringSerializerBase
-    {
-        private readonly JsonSerializerOptions _options;
-        private const string _applicationJson = "application/json";
-        public override string ContentType => _applicationJson;
+	public class JsonSerializer : StringSerializerBase
+	{
+		private readonly JsonSerializerOptions _options;
+		private const string _applicationJson = "application/json";
+		public override string ContentType => _applicationJson;
 
-        public JsonSerializer(JsonSerializerOptions options)
-        {
-            _options = options;
-        }
+		public JsonSerializer(JsonSerializerOptions options)
+		{
+			_options = options;
+		}
 
-        public override string SerializeToString(object obj)
-        {
-            if (obj == null)
-            {
-                return string.Empty;
-            }
-            if (obj is string str)
-            {
-                return str;
-            }
-            return System.Text.Json.JsonSerializer.Serialize(obj, obj.GetType(), _options);
-        }
+		public override string SerializeToString(object obj)
+		{
+			if (obj == null)
+			{
+				return string.Empty;
+			}
+			if (obj is string str)
+			{
+				return str;
+			}
+			return System.Text.Json.JsonSerializer.Serialize(obj, obj.GetType(), _options);
+		}
 
-        public override object Deserialize(Type type, string str)
-        {
-            if (type == typeof(string))
-            {
-                return str;
-            }
-            if (string.IsNullOrEmpty(str))
-            {
-                return null;
-            }
-            return System.Text.Json.JsonSerializer.Deserialize(str, type, _options);
-        }
-    }
+		public override object Deserialize(Type type, string str)
+		{
+			if (type == typeof(string))
+			{
+				return str;
+			}
+			if (string.IsNullOrEmpty(str))
+			{
+				return null;
+			}
+			return System.Text.Json.JsonSerializer.Deserialize(str, type, _options);
+		}
+	}
 }
