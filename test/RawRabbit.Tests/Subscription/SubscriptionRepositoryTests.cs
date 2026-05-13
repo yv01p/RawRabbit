e78d9d5898283b76e51e14acbf4cc152fb133e9b
Peter Zadrozny <246755631+yv01p@users.noreply.github.com>
2026-05-13 22:45:01 +0000

    add wave 3 subscription tests

---
+++ test/RawRabbit.Tests/Subscription/SubscriptionRepositoryTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using RawRabbit.Subscription;
+using Xunit;
+
+namespace RawRabbit.Tests.Subscription
+{
+	public class SubscriptionRepositoryTests
+	{
+		[Fact]
+		public void Ctor_Parameterless_GetAllReturnsEmpty()
+		{
+			var repo = new SubscriptionRepository();
+
+			var result = repo.GetAll();
+
+			Assert.Empty(result);
+		}
+
+		[Fact]
+		public void Add_SingleSubscription_GetAllReturnsOne()
+		{
+			var repo = new SubscriptionRepository();
+			var mockSub = Mock.Of<ISubscription>();
+
+			repo.Add(mockSub);
+
+			var result = repo.GetAll();
+			Assert.Single(result);
+			Assert.Same(mockSub, result[0]);
+		}
+
+		[Fact]
+		public void GetAll_ReturnsSnapshot_IndependentOfBagMutations()
+		{
+			var repo = new SubscriptionRepository();
+			var mockSub1 = Mock.Of<ISubscription>();
+			repo.Add(mockSub1);
+
+			var firstSnapshot = repo.GetAll();
+			var mockSub2 = Mock.Of<ISubscription>();
+			repo.Add(mockSub2);
+			var secondSnapshot = repo.GetAll();
+
+			Assert.Single(firstSnapshot);
+			Assert.Equal(2, secondSnapshot.Count);
+		}
+
+		[Fact]
+		public void Add_ConcurrentSafety_AllItemsAdded()
+		{
+			var repo = new SubscriptionRepository();
+
+			Parallel.For(0, 100, _ =>
+			{
+				repo.Add(Mock.Of<ISubscription>());
+			});
+
+			var result = repo.GetAll();
+			Assert.Equal(100, result.Count);
+		}
+	}
+}
---
+++ test/RawRabbit.Tests/Subscription/SubscriptionTests.cs
@@ -0,0 +1,96 @@
+﻿using System.Threading;
+using Moq;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RawRabbit.Subscription;
+using RawRabbit.Tests.TestHelpers;
+using Xunit;
+
+namespace RawRabbit.Tests.Subscription
+{
+	public class SubscriptionTests
+	{
+		[Fact]
+		public void Ctor_EventingBasicConsumer_SetsProperties()
+		{
+			var channel = BrokerMocks.MakeChannel();
+			var consumer = BrokerMocks.MakeEventingConsumer(channel.Object);
+			consumer.HandleBasicConsumeOk("tag1");
+
+			var sub = new RawRabbit.Subscription.Subscription(consumer, "queue1");
+
+			Assert.True(sub.Active);
+			Assert.Equal("queue1", sub.QueueName);
+			Assert.Equal("tag1", sub.ConsumerTag);
+		}
+
+		[Fact]
+		public void Ctor_NonDefaultBasicConsumer_SkipsAssignment()
+		{
+			var mockConsumer = new Mock<IBasicConsumer>();
+			mockConsumer.Setup(c => c.Model).Returns(BrokerMocks.MakeChannel().Object);
+
+			var sub = new RawRabbit.Subscription.Subscription(mockConsumer.Object, "queue1");
+
+			Assert.True(sub.Active);
+			Assert.Null(sub.QueueName);
+			Assert.Null(sub.ConsumerTag);
+		}
+
+		[Fact]
+		public void Dispose_InvokesBasicCancel()
+		{
+			var channel = BrokerMocks.MakeChannel();
+			var consumer = BrokerMocks.MakeEventingConsumer(channel.Object);
+			consumer.HandleBasicConsumeOk("tag1");
+			var sub = new RawRabbit.Subscription.Subscription(consumer, "queue1");
+
+			sub.Dispose();
+
+			channel.Verify(c => c.BasicCancel("tag1"), Times.Once);
+			Assert.False(sub.Active);
+		}
+
+		[Fact]
+		public void Dispose_SkipsWhenChannelClosed()
+		{
+			var channel = BrokerMocks.MakeChannel();
+			channel.Setup(c => c.IsOpen).Returns(false);
+			var consumer = BrokerMocks.MakeEventingConsumer(channel.Object);
+			consumer.HandleBasicConsumeOk("tag1");
+			var sub = new RawRabbit.Subscription.Subscription(consumer, "queue1");
+
+			sub.Dispose();
+
+			channel.Verify(c => c.BasicCancel(It.IsAny<string>()), Times.Never);
+		}
+
+		[Fact]
+		public void Dispose_Idempotent_InvokesOnce()
+		{
+			var channel = BrokerMocks.MakeChannel();
+			var consumer = BrokerMocks.MakeEventingConsumer(channel.Object);
+			consumer.HandleBasicConsumeOk("tag1");
+			var sub = new RawRabbit.Subscription.Subscription(consumer, "queue1");
+
+			sub.Dispose();
+			sub.Dispose();
+
+			channel.Verify(c => c.BasicCancel("tag1"), Times.Once);
+		}
+
+		[Fact]
+		public void Active_Setter_UpdatesState()
+		{
+			var channel = BrokerMocks.MakeChannel();
+			var consumer = BrokerMocks.MakeEventingConsumer(channel.Object);
+			var sub = new RawRabbit.Subscription.Subscription(consumer, "queue1");
+
+			sub.Active = false;
+			Assert.False(sub.Active);
+
+			sub.Active = true;
+			Assert.True(sub.Active);
+		}
+	}
+}
